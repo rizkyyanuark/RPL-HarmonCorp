a970254308b5e81df82f572ae87e916c2b859de2
rizkyyanuark <rizkyyanuarkristianto@gmail.com>
2025-01-14 16:23:18 +0700

    fix: fix secret to get PROJECT_ID

--- prototyping/utils/firebase_config.py
+++ prototyping/utils/firebase_config.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 def get_secret(secret_name, project_id=None):
     client = secretmanager.SecretManagerServiceClient()
-    project_id = st.secret['PROJECT_ID']
+    project_id = st.secrets['PROJECT_ID']
     if not project_id:
         raise ValueError("PROJECT_ID environment variable is not set.")
     secret_version = f'projects/{project_id}/secrets/{secret_name}/versions/latest'
