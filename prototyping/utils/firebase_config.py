fd8e3a270b3d5cf97a4a0718cb2134b8a8207fd7
rizkyyanuark <rizkyyanuarkristianto@gmail.com>
2025-01-14 16:41:53 +0700

    fix: get secret

--- prototyping/utils/firebase_config.py
+++ prototyping/utils/firebase_config.py
@@ -4,13 +4,15 @@ import streamlit as st
 from dotenv import load_dotenv
 from google.cloud import secretmanager
 import json
+import os
 
 load_dotenv()
 
 
 def get_secret(secret_name, project_id=None):
     client = secretmanager.SecretManagerServiceClient()
-    project_id = st.secrets['PROJECT_ID']
+    project_id = st.secrets.get('PROJECT_ID', os.getenv(
+        'PROJECT_ID'))  # Ambil dari environment variable
     if not project_id:
         raise ValueError("PROJECT_ID environment variable is not set.")
     secret_version = f'projects/{project_id}/secrets/{secret_name}/versions/latest'
